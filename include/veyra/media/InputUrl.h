a00d00067ea348d8ccc358469368aa58764065e1
NiuNan9527 <a321132919@163.com>
2026-09-16 06:53:08 +0800

    lan: accept TCP as realtime network input

--- include/veyra/media/InputUrl.h
+++ include/veyra/media/InputUrl.h
@@ -6,14 +6,14 @@ namespace veyra::media {
 
 inline bool isRealtimeNetworkUrl(std::wstring_view path)
 {
-    return path.starts_with(L"udp://")
+    return path.starts_with(L"tcp://")\n        || path.starts_with(L"udp://")
         || path.starts_with(L"rtp://")
         || path.starts_with(L"srt://");
 }
 
 inline bool isRealtimeNetworkUrl(std::string_view path)
 {
-    return path.starts_with("udp://")
+    return path.starts_with("tcp://")\n        || path.starts_with("udp://")
         || path.starts_with("rtp://")
         || path.starts_with("srt://");
 }
