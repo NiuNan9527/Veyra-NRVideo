625d53d42b822a0fd02ee689c3518010aa237ada
NiuNan9527 <a321132919@163.com>
2026-09-16 05:24:15 +0800

    lan: add realtime network URL classification

---
+++ include/veyra/media/InputUrl.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string_view>
+
+namespace veyra::media {
+
+inline bool isRealtimeNetworkUrl(std::wstring_view path)
+{
+    return path.starts_with(L"udp://")
+        || path.starts_with(L"rtp://")
+        || path.starts_with(L"srt://");
+}
+
+inline bool isRealtimeNetworkUrl(std::string_view path)
+{
+    return path.starts_with("udp://")
+        || path.starts_with("rtp://")
+        || path.starts_with("srt://");
+}
+
+} // namespace veyra::media
