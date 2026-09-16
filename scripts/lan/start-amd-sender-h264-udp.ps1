182097808dae6c2da26b344b6a75dd5a41f7aac7
NiuNan9527 <a321132919@163.com>
2026-09-16 08:05:21 +0800

    lan: fix H.264 AMF parameter-set insertion for live startup

--- scripts/lan/start-amd-sender-h264-udp.ps1
+++ scripts/lan/start-amd-sender-h264-udp.ps1
@@ -29,7 +29,8 @@ Write-Host "Veyra H.264 AMF UDP timing test -> $TargetIp`:$Port"
     -g 15 `
     -bf 0 `
     -async_depth 1 `
-    -header_insertion_mode idr `
+    -header_spacing 15 `
+    -aud 1 `
     -mpegts_flags +resend_headers `
     -muxdelay 0 `
     -muxpreload 0 `
